6a221d28afaad397a5911886ca314428e7eb4218
Joseph LeGrand <jrlegrand@gmail.com>
2017-06-02 06:51:47 -0500

    Got playlist working

--- routes/localtop.js
+++ routes/localtop.js
@@ -1,4 +1,12 @@
 // Get local artists' top tracks
+// #######################################################
+// # NOTES TO BOB / JOEY
+// # Bob - you will probably have to use my client id / secret to make the playlis stuff work
+// # I will email it to you
+// #
+// #
+// #
+// ######################################################
 
 var express = require('express');
 var request = require('request');
@@ -10,11 +18,12 @@ var events = [];
 var tracks = [];
 var names = [];
 var ids = [];
+var playlist = [];
+var spotifyUserId = 'joeschmoey';
+var playlistBaseName = 'Music City Playlist';
 
 router.get('/', function(req, res) {
-  
-  events = tracks = names = ids = [];
-  
+
   let eventPromise = new Promise((resolve, reject) => {
     getLocalEvents(resolve);
   });
@@ -25,15 +34,31 @@ router.get('/', function(req, res) {
       getArtistIds(resolve);
     });
     
-    idPromise.then(() => {
-      let trackPromise = new Promise((resolve, reject) => {
+	  idPromise.then(() => {
+        let topTracksPromise = new Promise((resolve, reject) => {
         getTopTracks(resolve);
       });
       
-      trackPromise.then(() => {
-        res.send(tracks);
-      });
+      topTracksPromise.then(() => {
+		let playlistPromise = new Promise((resolve, reject) => {
+	      createPlaylist(resolve);
+		});
+		  
+		playlistPromise.then(() => {
+			console.log('playlistID', playlist[0]);
+		  let addTracksPromise = new Promise((resolve, reject) => {
+	        addTracks(resolve);
+		  });
+		  
+	      addTracksPromise.then(() => {
+			res.send(playlist);
+          });
+
+		});
+      })
+	  
     });
+
   });
 });
 
@@ -97,17 +122,58 @@ function getTopTracks(callback) {
   });
 }
 
-function getTopTrack(id, callback){  
+function getTopTrack(id, callback){
   var ops = {
     url: "https://api.spotify.com/v1/artists/" + id +"/top-tracks?country=US",
     headers: { 'Authorization': 'Bearer ' + secrets.token() },
   }
   request.get(ops, (err, res, body) => {    
-    try{ tracks.push(JSON.parse(body).tracks[0]); }
-    catch(e) {}
+	var track = JSON.parse(body).tracks[0] || null;
+	if (track) {
+		try{ tracks.push(track); }
+		catch(e) {}
+	}
     callback();
   })
   
 }
+
+function createPlaylist(callback){
+  var ops = {
+	 url: "https://api.spotify.com/v1/users/" + spotifyUserId + "/playlists",
+	 json: { "name": playlistBaseName, "public": false },
+	 headers: { 'Authorization': 'Bearer ' + secrets.token(), 'Content-Type': 'application/json' },
+	}
+  request.post(ops, (err, res, body) => {
+    playlist.push(body);
+    callback();
+  }) 
+}
+
+
+function addTracks(callback) {
+
+
+  var trackUris = tracks.map((track) => {
+	if (track) return track.uri;
+  });
+
+  console.log('trackUris', trackUris);
+  
+  var ops = {
+	 url: "https://api.spotify.com/v1/users/" + spotifyUserId + "/playlists/" + playlist[0].id + "/tracks",
+	 json: { "uris": trackUris },
+	 headers: { 'Authorization': 'Bearer ' + secrets.token(), 'Content-Type': 'application/json' },
+	}
+  request.post(ops, (err, res, body) => {
+	//console.log('playlisterr', err);
+	//console.log('playlistres', res);
+	//console.log('playlistbody', body);
+	 
+    callback();
+  }) 
+  
+}
+
 
 module.exports = router;
